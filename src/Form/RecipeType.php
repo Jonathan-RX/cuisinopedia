699865e8f20fd162a815879c043970ddb45ab5aa
JonathanRX <jonathan.ravix@gmail.com>
2021-01-08 08:08:23 +0100

    Add recipe Crud

---
+++ src/Form/RecipeType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recipe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('time_prepare')
+            ->add('time_cooking')
+            ->add('time_rest')
+            ->add('ingredients')
+            ->add('tools')
+            ->add('steps')
+            ->add('id_author')
+            ->add('created_time')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}
